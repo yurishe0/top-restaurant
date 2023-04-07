181dcde5d5f7960eef068319ef53164346961318
yurishe0 <yurishe00@gmail.com>
2023-04-07 12:23:45 +0200

    Fix a bug that caused created elements with no class to crash the site

--- src/newElement.js
+++ src/newElement.js
@@ -1,7 +1,9 @@
 export const newElement = (elementName, textContent, attribute, attributeValue, ...classNames) => {
     const element = document.createElement(elementName);
     for(let i = 0; i < classNames.length; i++) {
-        element.classList.add(classNames[i]);
+        if(classNames[i] != "") {
+            element.classList.add(classNames[i]);
+        }
     }
     if(attribute != "") {
         element.setAttribute(attribute, attributeValue);
