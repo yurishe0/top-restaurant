a04f688c91352124f1c10f5b827a71761ba072d9
yurishe0 <yurishe00@gmail.com>
2023-04-07 11:50:37 +0200

    Create a generator from DOM elements to improve code quality and readability

---
+++ src/newElement.js
@@ -0,0 +1,13 @@
+export const newElement = (elementName, textContent, attribute, attributeValue, ...classNames) => {
+    const element = document.createElement(elementName);
+    for(let i = 0; i < classNames.length; i++) {
+        element.classList.add(classNames[i]);
+    }
+    if(attribute != "") {
+        element.setAttribute(attribute, attributeValue);
+    }
+    element.textContent = textContent;
+
+    return element;
+}
+
