9f033ea91b1f814445cfeadef526b7c652327d16
yurishe0 <yurishe00@gmail.com>
2023-04-07 12:02:54 +0200

    Refactor the code using the newElement function

--- src/page-load.js
+++ src/page-load.js
@@ -1,81 +1,60 @@
+import { newElement } from "./newElement.js";
+
 export const pageLoad = () => {
-    const content = document.querySelector('#content');
-    const mainContainer = document.createElement('div');
-    const heroImageContainer = document.createElement('div');
-    const contentContainer = document.createElement('div');
-    const navbar = document.createElement('div');
-    const about = document.createElement('div');
-    const historyContainer = document.createElement('div');
-    const aboutImageContainer = document.createElement('div');
-    const aboutImage = document.createElement('img');
+  const content = document.querySelector("#content");
+
+  // *** ELEMENT CREATION
+  // SYNTAX:
+  // const x = newElement(elementName, textContent, attribute, attributeValue, ...classNames);
+  const mainContainer = newElement("div", "", "", "", "main-container");
+  const heroImageContainer = newElement("div", "", "", "", "hero-image__container")
+  const contentContainer = newElement("div", "", "", "", "content-container");
+  const navbar = newElement("div", "", "", "", "navbar");
+  const about = newElement("div", "", "", "", "about-item", "about-us");
+  const historyContainer = newElement("div", "", "", "", "about-item", "history-container")
+  const aboutImageContainer = newElement("div", "", "", "", "about-item", "aboutImageContainer");
+
+  const aboutImage = newElement("img", "", "src", "https://arquitecturaviva.com/assets/uploads/obras/43429/av_imagen_vertical.jpeg?h=1410ee69", "");
+
+  const pizzarioBig = newElement("h1", "PIZZARIO", "", "", "");
+  const pizzarioSmall = newElement("h2", "Pizzario", "", "", "");
+  const aboutUs = newElement("h3", "ABOUT US", "", "", "");
+  const history = newElement("h3", "history", "", "", "");
+
+  const paragraph1 = newElement("p", "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui beatae nulla, minus debitis sint veniam. Doloremque, praesentium ea? Repudiandae nesciunt numquam culpa rerum iure neque repellendus doloribus eum quia se.", "", "", "");
+  const paragraph2 = newElement("p", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque incidunt architecto, est dignissimos dolorum eos accusantium.", "", "", "");
+  const paragraph3 = newElement("p", "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui beatae nulla, minus debitis sint veniam. Doloremque, praesentium ea? Repudiandae nesciunt numquam culpa rerum iure neque repellendus doloribus eum quia sed laboriosam, temporibus, deleniti voluptatem atque error quae dolore molestiae aliquid maxime esse! Quo ut tempore distinctio necessitatibus perspiciatis alias facilis vitae.", "", "", "");
+  const paragraph4 = newElement("p", "Lorem ipsum dolor!", "", "", "");
+
+  const link1 = newElement("a", "MENU", "", "", "");
+  const link2 = newElement("a", "LOCATION", "", "", "");
+  const link3 = newElement("a", "CONTACT", "", "", "");
+
+  // ***
 
 
-    const pizzarioBig = document.createElement('h1');
-    const pizzarioSmall = document.createElement('h2');
-    const aboutUs = document.createElement('h3');
-    const history = document.createElement('h3');
-    const paragraph1 = document.createElement('p');
-    const paragraph2 = document.createElement('p');
-    const paragraph3 = document.createElement('p');
-    const paragraph4 = document.createElement('p');
-    const link1 = document.createElement('a');
-    const link2 = document.createElement('a');
-    const link3 = document.createElement('a');
+  // APPENDING DOM ELEMENTS
+  heroImageContainer.appendChild(pizzarioBig);
+  mainContainer.appendChild(heroImageContainer);
+  navbar.appendChild(pizzarioSmall);
+  navbar.appendChild(link1);
+  navbar.appendChild(link2);
+  navbar.appendChild(link3);
+  mainContainer.appendChild(navbar);
 
-    mainContainer.classList.add("main-container");
-    heroImageContainer.classList.add("hero-image__container");
-    contentContainer.classList.add("content-container");
-    navbar.classList.add("navbar");
-    about.classList.add("about-item")
-    about.classList.add("about-us");
+  about.appendChild(aboutUs);
+  about.appendChild(paragraph1);
+  about.appendChild(paragraph2);
+  contentContainer.appendChild(about);
 
-    aboutImage.setAttribute('src', 'https://arquitecturaviva.com/assets/uploads/obras/43429/av_imagen_vertical.jpeg?h=1410ee69');
-    aboutImageContainer.classList.add('about-item');
-    aboutImageContainer.classList.add('about-image-container');
-    historyContainer.classList.add('about-item');
-    historyContainer.classList.add('history-container');
+  aboutImageContainer.appendChild(aboutImage);
+  contentContainer.appendChild(aboutImageContainer);
 
+  historyContainer.appendChild(history);
+  historyContainer.appendChild(paragraph3);
+  historyContainer.appendChild(paragraph4);
+  contentContainer.appendChild(historyContainer);
 
-    pizzarioBig.textContent = "PIZZARIO";
-    pizzarioSmall.textContent = "Pizzario";
-    aboutUs.textContent = "ABOUT US";
-    history.textContent = "HISTORY";
-    link1.textContent = "MENU";
-    link2.textContent = "LOCATION";
-    link3.textContent = "CONTACT";
-    paragraph1.textContent = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui beatae nulla, minus debitis sint veniam. Doloremque, praesentium ea? Repudiandae nesciunt numquam culpa rerum iure neque repellendus doloribus eum quia se.";
-    paragraph2.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque incidunt architecto, est dignissimos dolorum eos accusantium.";
-    paragraph3.textContent = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui beatae nulla, minus debitis sint veniam. Doloremque, praesentium ea? Repudiandae nesciunt numquam culpa rerum iure neque repellendus doloribus eum quia sed laboriosam, temporibus, deleniti voluptatem atque error quae dolore molestiae aliquid maxime esse! Quo ut tempore distinctio necessitatibus perspiciatis alias facilis vitae.";
-    paragraph4.textContent = "Lorem ipsum dolor!";
-
-
-    heroImageContainer.appendChild(pizzarioBig);
-    mainContainer.appendChild(heroImageContainer);
-    navbar.appendChild(pizzarioSmall);
-    navbar.appendChild(link1);
-    navbar.appendChild(link2);
-    navbar.appendChild(link3);
-    mainContainer.appendChild(navbar);
-
-    about.appendChild(aboutUs);
-    about.appendChild(paragraph1);
-    about.appendChild(paragraph2);
-    contentContainer.appendChild(about);
-
-    aboutImageContainer.appendChild(aboutImage);
-    contentContainer.appendChild(aboutImageContainer);
-
-    historyContainer.appendChild(history);
-    historyContainer.appendChild(paragraph3);
-    historyContainer.appendChild(paragraph4);
-    contentContainer.appendChild(historyContainer);
-
-
-
-
-    mainContainer.appendChild(contentContainer);
-    content.appendChild(mainContainer);
-}
-
-
-
+  mainContainer.appendChild(contentContainer);
+  content.appendChild(mainContainer);
+};
