4ba4588b66239415e633bfa45542a0e7a75c0162
yurishe0 <yurishe00@gmail.com>
2023-04-13 21:56:58 +0200

    Implement logic for switching between pages via the navbar

--- src/index.js
+++ src/index.js
@@ -2,4 +2,43 @@ import { mainPage } from "./main-page.js";
 import { about } from "./about.js";
 
 mainPage();
+
+// const link1 = document.querySelector('#about-us');
+// link1.addEventListener('click', () => {
+    //     console.log('a');
+    // });
+
+    const mainContent = document.querySelector('.main-content');
+    const navbarLinks = document.querySelectorAll('.navbar a');
+
+navbarLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        let linkName = link.textContent;
+        changePage(linkName);
+    });
+})
+
+function changePage(page) {
+    switch(page) {
+        case "ABOUT US":
+            clearPage();
+            about();
+            break;
+        case "MENU":
+            clearPage();
+            break;
+        case "LOCATION":
+            clearPage();
+            break;
+        case "CONTACT":
+            clearPage();
+            break;
+    }
+}
+
+const clearPage = () => {
+    mainContent.innerHTML = '';
+}
+
 about();
+
