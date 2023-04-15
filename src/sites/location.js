ee266bdcea00300abbbdfcabfa083ca3d0eda13e
yurishe0 <yurishe00@gmail.com>
2023-04-15 18:43:38 +0200

    Adjust iframe size

--- src/sites/location.js
+++ src/sites/location.js
@@ -10,7 +10,7 @@ export const location = () => {
 
     textContainer.appendChild(locationHeader);
     textContainer.appendChild(locationText);
-    mapContainer.innerHTML += '<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d229620.79658545903!2d-74.16796418157318!3d40.87211261357002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2spl!4v1681574407950!5m2!1sen!2spl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+    mapContainer.innerHTML += '<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d229620.79658545903!2d-74.16796418157318!3d40.87211261357002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2spl!4v1681574407950!5m2!1sen!2spl" width="70%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
     mainContent.appendChild(textContainer);
     mainContent.appendChild(mapContainer);
 }
